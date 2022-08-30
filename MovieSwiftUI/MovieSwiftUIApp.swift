9b7cee69df4ecd3af2c900f76c1f5fb35abeb966
Cuongvx <cuongvx@mobio.io>
2022-08-30 23:36:02 +0700

    Initial Commit

---
+++ MovieSwiftUI/MovieSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftUIApp.swift
+//  MovieSwiftUI
+//
+//  Created by sun on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
