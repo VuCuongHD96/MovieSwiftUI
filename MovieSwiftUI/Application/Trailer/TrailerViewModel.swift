1c821f5a6058f80af6183e2fb4120d960050cd89
VuCuongHD96 <xuancuonghd1102@gmail.com>
2023-11-20 20:36:06 +0700

    trailer add action

--- MovieSwiftUI/Application/Trailer/TrailerViewModel.swift
+++ MovieSwiftUI/Application/Trailer/TrailerViewModel.swift
@@ -6,18 +6,24 @@
 //
 
 import Combine
+import YouTubeKit
 
 struct TrailerViewModel {
     
     struct Input {
         var loadTrigger = PassthroughSubject<Void, Never>()
+        var dismissAction = PassthroughSubject<Void, Never>()
+        var openTrailerAction = PassthroughSubject<Trailer, Never>()
     }
     
     class Output: ObservableObject {
         @Published var trailerList = [Trailer]()
+        @Published var trailer: Trailer?
+        @Published var alertMessage = AlertMessage()
     }
     
     let trailerUseCase: TrailerUseCaseType
+    let trailerNavigator: TrailerNavigator
     let movie: Movie
 }
 
@@ -25,6 +31,7 @@ extension TrailerViewModel: ViewModel {
     
     func transform(_ input: Input, cancelBag: CancelBag) -> Output {
         let output = Output()
+        let errorTracker = ErrorTracker()
         
         input.loadTrigger
             .flatMap { _ in
@@ -32,6 +39,31 @@ extension TrailerViewModel: ViewModel {
                     .asDriver()
             }
             .assign(to: \.trailerList, on: output)
+            .store(in: cancelBag)
+        
+        input.dismissAction
+            .sink { _ in
+                trailerNavigator.dismiss()
+            }
+            .store(in: cancelBag)
+
+        input.openTrailerAction
+            .flatMap { trailer in
+                YoutubeDownloader.shared
+                    .fetchYouTubeStream(videoID: trailer.key)
+                    .trackError(errorTracker)
+                    .asDriver()
+            }
+            .sink { stream in
+                trailerNavigator.toVideoScreen(stream: stream)
+            }
+            .store(in: cancelBag)
+        
+        errorTracker
+            .map {
+                AlertMessage(error: $0)
+            }
+            .assign(to: \.alertMessage, on: output)
             .store(in: cancelBag)
         
         return output
